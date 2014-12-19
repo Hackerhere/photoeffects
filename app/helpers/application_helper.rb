ffd1aee07605e10e625d2c81219f74af827e53f4
Lokesh Sharma <lokeshhsharma@gmail.com>
2014-12-19 18:16:45 +0000

    Convert to ImageEffect

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns full title for pages
   def full_title(page_title = '')
-    base_title = 'Ruby on Rails Tutorial Sample App'
+    base_title = 'ImageEffect Sample App'
     if page_title.empty?
       base_title
     else
